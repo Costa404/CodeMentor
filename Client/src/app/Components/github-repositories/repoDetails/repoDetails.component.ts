a4f8d7b4360ff340e37bbff32247a0297a5ec20b
nuno <nuno-costa17@hotmail.com>
2025-03-11 23:59:10 +0000

    docs  frontend

--- Client/src/app/Components/github-repositories/repoDetails/repoDetails.component.ts
+++ Client/src/app/Components/github-repositories/repoDetails/repoDetails.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { GithubRepoService } from '../github-repo.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -15,8 +15,11 @@ export class RepoDetailsComponent implements OnInit {
   repoDetails: any = null;
   repoFiles: any[] = [];
   loading: boolean = false;
-
-  constructor(private githubRepoService: GithubRepoService) {}
+  selectedFile: any = null; 
+  constructor(
+    private githubRepoService: GithubRepoService,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.loadRepoDetails();
@@ -25,24 +28,37 @@ export class RepoDetailsComponent implements OnInit {
   loadRepoDetails(): void {
     this.loading = true;
 
+    // Carrega os detalhes do repositório
     this.githubRepoService.getRepoDetails(this.username, this.repo).subscribe(
       (details) => {
         this.repoDetails = details;
+        this.cdRef.detectChanges(); // Força a atualização da UI
       },
       (error) => {
         console.error('Erro ao obter detalhes do repositório:', error);
+        this.loading = false;
+        this.cdRef.detectChanges(); // Força a atualização da UI em caso de erro
       }
     );
 
+    // Carrega os arquivos do repositório
     this.githubRepoService.getRepoFiles(this.username, this.repo).subscribe(
       (files) => {
-        this.repoFiles = files;
+        this.repoFiles = files; // Atualiza a lista de arquivos
         this.loading = false;
+        this.cdRef.detectChanges(); // Força a atualização da UI
       },
       (error) => {
         console.error('Erro ao obter arquivos do repositório:', error);
         this.loading = false;
+        this.cdRef.detectChanges(); 
       }
     );
+  }
+
+
+  selectFile(file: any): void {
+    this.selectedFile = file;
+    this.cdRef.detectChanges(); /
   }
 }
